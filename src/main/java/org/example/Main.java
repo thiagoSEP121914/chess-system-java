74d3aaa35c62832c2613a2f672638cb93993bd28
Thiago <thiagodacostaalves35@gmail.com>
2025-01-13 20:40:13 -0300

    Handling eith captured pieces

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 import chess.*;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -11,21 +13,29 @@ public class Main {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             ChessMatch chessMatch = new ChessMatch();
+            List<ChessPiece> captured = new ArrayList<>();
+
             while (true) {
               try {
                   UI.clearScreen();
-                  UI.printMatch(chessMatch);
+                  UI.printMatch(chessMatch, captured);
                   System.out.println();
                   System.out.print("Source: ");
                   ChessPosition source = UI.readChessPosition(sc);
 
                   boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-                    UI.clearScreen();
+                  UI.clearScreen();
                   UI.printBoard(chessMatch.getPieces(), possibleMoves);
                   System.out.println();
                   System.out.print("Target: ");
                   ChessPosition target = UI.readChessPosition(sc);
+
                   ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                  if (capturedPiece != null) {
+                      captured.add(capturedPiece);
+                  }
+
               }catch (ChessException e) {
                   System.out.println(e.getMessage());
                   sc.nextLine();
